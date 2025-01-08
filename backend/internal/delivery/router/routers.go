b08769a683cb456fb5cbe26e23ec4803663d4cdf
NemoFlip <artgamer3163@gmail.com>
2025-01-08 16:11:21 +0300

    made: router for user's preferences

--- backend/internal/delivery/router/routers.go
+++ backend/internal/delivery/router/routers.go
@@ -1,5 +1,11 @@
 package router
 
-func InitRouting() {
+import (
+	"Christmas_prj/backend/internal/delivery/handlers"
+	"Christmas_prj/backend/pkg/log"
+	"github.com/gin-gonic/gin"
+)
 
+func InitRouting(r *gin.Engine, logger *log.Logger, apiServer *handlers.APIServer) {
+	r.POST("/api/users/data", apiServer.PostUserInfo)
 }
