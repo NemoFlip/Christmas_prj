3960015c0b4b6fa9b55143a2d9d7d2481945200e
NemoFlip <artgamer3163@gmail.com>
2025-01-09 22:46:59 +0300

    made: http request to mock ml server

--- backend/internal/delivery/handlers/api.go
+++ backend/internal/delivery/handlers/api.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"Christmas_prj/backend/internal/entity"
 	"Christmas_prj/backend/pkg/log"
+	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -19,8 +21,9 @@ func NewAPIServer(logger *log.Logger) *APIServer {
 // @Description post request from user for present recommendation
 // @Tags API
 // @Accept json
+// @Produce json
 // @Param user body entity.User true "user's preferences"
-// @Success 200 {object} string "user's preferences has been accepted"
+// @Success 200 {object} map[string]string "user's preferences has been accepted"
 // @Failure 400 {object} string "invalid user's data"
 // @Router /api/users/data [post]
 func (s *APIServer) PostUserInfo(ctx *gin.Context) {
@@ -30,5 +33,28 @@ func (s *APIServer) PostUserInfo(ctx *gin.Context) {
 		ctx.Writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		s.logger.ErrorLogger.Error().Msgf("unable to convert user struct to json: %s", err)
+		ctx.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	r := bytes.NewBuffer(userJSON)
+	mlURL := "https://ml-model-recommendation"
+	resp, err := http.Post(mlURL, "application/json", r) //TODO: replace url
+	if err != nil {
+		s.logger.ErrorLogger.Error().Msgf("unable to send request to ml: %s", err)
+		ctx.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		s.logger.ErrorLogger.Error().Msgf("ML model responded with status: %d", resp.StatusCode)
+		ctx.Writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": resp.Status,
+	})
 }
