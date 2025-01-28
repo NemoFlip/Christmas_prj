2b61c1620ac60b14b4af9e6aa013fb25aa1fcf17
NemoFlip <artgamer3163@gmail.com>
2025-01-28 12:20:49 +0300

    fix: backend-frontend connection

--- backend/internal/delivery/app.go
+++ backend/internal/delivery/app.go
@@ -15,11 +15,26 @@ import (
 // @BasePath /
 func StartServer(logger *log.Logger, giftServer *handlers.GiftServer) {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.InitRouting(r, giftServer)
 
 	if err := r.Run(":8080"); err != nil {
 		logger.ErrorLogger.Fatal().Msgf("unable to run server on port(8080): %s", err)
+	}
+}
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
